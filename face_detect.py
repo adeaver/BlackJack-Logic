c53c1499b1c51f9b41aa5718a3dafd6d0facd645
adeaver <deaver.andrew@gmail.com>
2016-12-04 17:13:10 -0500

    Running faster

--- face_detect.py
+++ face_detect.py
@@ -27,37 +27,39 @@ cap = PiRGBArray(camera, size=(320, 240))
 
 box_width = 10
 
-while state != 9:
-    for capture in camera.capture_continuous(cap, format="bgr", use_video_port=True):
-      print "Capturing..."
-      frame = capture.array
-      should_send = True
-      #ret, frame = cap.read()
+for capture in camera.capture_continuous(cap, format="bgr", use_video_port=True):
+  print "Capturing..."
+  frame = capture.array
+  should_send = True
+  #ret, frame = cap.read()
 
-      height = frame.shape[0]
-      width = frame.shape[1]
+  height = frame.shape[0]
+  width = frame.shape[1]
 
-      lower_bound = (width/2)-box_width
-      upper_bound = (width/2)+box_width
+  lower_bound = (width/2)-box_width
+  upper_bound = (width/2)+box_width
 
-      faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20,20))
-      for (x,y,w,h) in faces:
-          if(x+w/2 >= lower_bound and x+w/2 <= upper_bound):
-              print "Detected new player"
-              scan_state = send_and_receive(spi, 8)
-              should_send = False
+  faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20,20))
+  for (x,y,w,h) in faces:
+      if(x+w/2 >= lower_bound and x+w/2 <= upper_bound):
+          print "Detected new player"
+          scan_state = send_and_receive(spi, 8)
+          should_send = False
 
-          cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255))
-      cv2.rectangle(frame, (lower_bound, 0), (upper_bound, height), (0, 255, 0))
+      cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255))
+  cv2.rectangle(frame, (lower_bound, 0), (upper_bound, height), (0, 255, 0))
 
-      if(should_send):
-          scan_state = send_and_receive(spi, 7)
+  if(should_send):
+      scan_state = send_and_receive(spi, 7)
 
-        #Display the resulting frame
-        #cv2.imshow('frame',frame)
-      if cv2.waitKey(1) & 0xFF == ord('q'):
-          break
+    #Display the resulting frame
+    #cv2.imshow('frame',frame)
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
 
-      cap.truncate(0)
+  cap.truncate(0)
+
+  if scan_state == 9:
+    break
 
 print "Done!"
