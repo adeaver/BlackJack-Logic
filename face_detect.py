959482707ffb2a49b049def78c970a97714af82a
adeaver <deaver.andrew@gmail.com>
2016-12-10 15:40:41 -0500

    Face Detection

--- face_detect.py
+++ face_detect.py
@@ -1,8 +1,13 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+import RPi.GPIO as GPIO
 import numpy as np
 import cv2, time
 import serial.tools.list_ports, serial, time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN)
+started = False
 
 def find_arduino():
     ports = list(serial.tools.list_ports.comports())
@@ -45,58 +50,63 @@ ser = serial.Serial(port=port, baudrate=9600, timeout=.01)
 ser.setRTS(True)
 ser.setRTS(False)
 
+while True:
+    started = GPIO.input(17)
+    while started:
+        state = 0
+        count_states = 0
 
-state = 0
-count_states = 0
+        face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt.xml')
+        camera = PiCamera()
 
-face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt.xml')
-camera = PiCamera()
+        camera.resolution = (640, 480)
+        camera.framerate = 30 
 
-camera.resolution = (640, 480)
-camera.framerate = 30 
+        cap = PiRGBArray(camera, size=(640, 480))
 
-cap = PiRGBArray(camera, size=(640, 480))
+        box_width = 30
 
-box_width = 30
+        try:
+            for capture in camera.capture_continuous(cap, format="bgr", use_video_port=True):
+                count_states += 1
+                print "Capturing..."
+                frame = capture.array
+                should_send = True
+                #ret, frame = cap.read()
 
-try:
-    for capture in camera.capture_continuous(cap, format="bgr", use_video_port=True):
-        count_states += 1
-        print "Capturing..."
-        frame = capture.array
-        should_send = True
-        #ret, frame = cap.read()
+                height = frame.shape[0]
+                width = frame.shape[1]
 
-        height = frame.shape[0]
-        width = frame.shape[1]
+                lower_bound = (width/2)-box_width
+                upper_bound = (width/2)+box_width
 
-        lower_bound = (width/2)-box_width
-        upper_bound = (width/2)+box_width
+                faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20,20))
+                for (x,y,w,h) in faces:
+                  if(x+w/2 >= lower_bound and x+w/2 <= upper_bound):
+                      print "Detected new player"
+                      scan_state = send_and_receive(ser, "8")
+                      should_send = False
 
-        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20,20))
-        for (x,y,w,h) in faces:
-          if(x+w/2 >= lower_bound and x+w/2 <= upper_bound):
-              print "Detected new player"
-              scan_state = send_and_receive(ser, "8")
-              should_send = False
+                  cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255))
+                cv2.rectangle(frame, (lower_bound, 0), (upper_bound, height), (0, 255, 0))
 
-          cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255))
-        cv2.rectangle(frame, (lower_bound, 0), (upper_bound, height), (0, 255, 0))
+                if(should_send):
+                  scan_state = send_and_receive(ser, "7")
 
-        if(should_send):
-          scan_state = send_and_receive(ser, "7")
+                #Display the resulting frame
+                #cv2.imshow('frame',frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                  break
 
-        #Display the resulting frame
-        #cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-          break
+                cap.truncate(0)
 
-        cap.truncate(0)
-
-        if scan_state == 9:
+                if scan_state == 9:
+                    break
+        except KeyboardInterrupt:
+            ser.close()
+        except:
             break
-except KeyboardInterrupt:
-    ser.close()
+    started = False
 
 ser.close()
 
